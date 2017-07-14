b4ac67c1852b6c2640464b95555cf2687b9b2ede
Yunjikung <yunjikung@gmail.com>
2017-07-14 09:37:36 +0700

    Initial Commit

---
+++ PodTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PodTest
+//
+//  Created by Narasret Weeraprasert on 7/14/2560 BE.
+//  Copyright © 2560 yunjikung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
